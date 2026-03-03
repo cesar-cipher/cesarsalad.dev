c2cbb027c4ef0cbc3c8623cca7f8c75dbdd04f1a
Cesar Cano <me@cesarsalad.dev>
2026-03-02 18:40:58 -0800

    style: added color palettes to vars.css and began CSS for UI layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import './App.css'
-import styles from "./card/Card.module.css";
+import appStyles from "./App.module.css";
+import cardStyles from "./card/Card.module.css";
+
 import CardSection from "./card/CardSection.tsx";
+
 import { sections } from "./data/CardEntryData.tsx";
 
 
 function App() {
   return (
-    <>
+    <div className={appStyles.App}>
       {/* NavBar will go here */}
 
-      <div className={styles.cardDeck}>
+      <div className={cardStyles.cardDeck}>
         {/* It's cleaner to put the data in a separate file, 
             so it can be edited quickly without modifying code. */}
         {sections.map((section) => (
           <CardSection key={section.title} {...section} />
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
